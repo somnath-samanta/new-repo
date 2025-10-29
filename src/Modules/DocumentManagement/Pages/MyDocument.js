4238f481e528af0a791fbd6336c573ea2d452000
somnath <somnath.samanta@mettletech.in>
2025-10-29 12:01:14 +0530

    UI fixing

--- src/Modules/DocumentManagement/Pages/MyDocument.js
+++ src/Modules/DocumentManagement/Pages/MyDocument.js
@@ -1168,7 +1168,7 @@ const styles = StyleSheet.create({
         // maxWidth: '100%',
         width: '100%',
         height: '100%',
-        objectFit: 'cover',
+        objectFit: 'contain',
         resizeMode: 'contain',
 
         // objectFit:'cover',
--- src/Modules/DocumentManagement/Pages/ThirdPartyDocument.js
+++ src/Modules/DocumentManagement/Pages/ThirdPartyDocument.js
@@ -1188,7 +1188,7 @@ const styles = StyleSheet.create({
         // maxWidth: '100%',
         width: '100%',
         height: '100%',
-        objectFit: 'cover',
+        objectFit: 'contain',
         resizeMode: 'contain',
 
         // objectFit:'cover',

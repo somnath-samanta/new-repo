44d3a4b3d61bc722e6311ae76bd19d67ded7265e
somnath <somnath.samanta@mettletech.in>
2025-10-29 19:03:58 +0530

    UI fixing

--- src/Modules/DocumentManagement/Components/FileUploadAndTakePhoto.jsx
+++ src/Modules/DocumentManagement/Components/FileUploadAndTakePhoto.jsx
@@ -865,10 +865,10 @@ const styles = StyleSheet.create({
 
   },
   documentTypeBoxLeft: {
-    width: Platform.OS === 'ios' ? '50%' : '47%',
+    width: Platform.OS === 'ios' ? '50%' : '49%',
   },
   documentTypeBoxRight: {
-    width: Platform.OS === 'ios' ? '47%' : '52%',
+    width: Platform.OS === 'ios' ? '47%' : '50%',
   },
   documentTypeSecPicker: {
     // borderBottomWidth: 1,
